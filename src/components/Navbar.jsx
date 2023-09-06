77f8deef5815c81c544ed83b006d080d463b9d6c
Rakshit Rabugotra <rakshit.rabugotra000@gmail.com>
2023-09-06 13:13:25 +0530

    Add additional page links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,13 @@ function Navbar() {
         <div className="navigation">
             <nav>
                 <ul className="nav-type">
+
                 <li><a href="https://codepen.io/EneergeticTomy/" target="_blank" className="active">Home</a></li>
-                <li><a href="https://codepen.io/TomikaBoy/full/LqMpaE" target="_blank" className="active1">About</a></li>
-                <li><a href="https://codepen.io/" target="_blank" className="active2">Contact</a></li>
-                <li><a href="https://w3schools.com/" target="_blank" className="active3">Support</a></li>
+                <li><a href="https://codepen.io/TomikaBoy/full/LqMpaE" target="_blank" className="active">About</a></li>
+                <li><a href="https://codepen.io/" target="_blank" className="active">Gallery</a></li>
+                <li><a href="https://w3schools.com/" target="_blank" className="active">Events</a></li>
+                <li><a href="https://w3schools.com/" target="_blank" className="active">Blog</a></li>
+                <li><a href="https://w3schools.com/" target="_blank" className="active">Contact</a></li>
                 <div className="line"></div>
                 <li><a><i className="fa fa-search" onclick="show()">
                     </i></a></li>
