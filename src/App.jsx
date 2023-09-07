e13d5f60bc934517f8e0a14ee2cd3f1bbe99f216
unknown <deepanshu.9dd@gmail.com~>
2023-09-07 13:53:25 +0530

    Add cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import Card from './components/Card'
 
 function App() {
 
   return (
     <>
       <Navbar/>
+      <div className='content'>
+      <Card/>
+      </div>
     </>
   )
 }
