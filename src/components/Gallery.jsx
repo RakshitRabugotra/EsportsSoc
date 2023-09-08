8c80ef1a02ed521a75602cb3535e870880bbd642
Rakshit Rabugotra <rakshit.rabugotra000@gmail.com>
2023-09-08 13:18:58 +0530

    Add Gallery

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,22 @@
+import PhotoAlbum from "react-photo-album";
+
+const photos = [
+    {
+        src: "/images/image1.jpg",
+        width: 800,
+        height: 600
+    },
+    {
+        src: "/images/image2.jpg",
+        width: 1600,
+        height: 900
+    }
+];
+
+function Gallery() {
+    return (
+        <PhotoAlbum layout="rows" photos={photos} />
+    )
+}
+
+export default Gallery;
