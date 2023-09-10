41c784a5917f7820bf6aae264da9aeb5b83f2692
unknown <deepanshu.9dd@gmail.com~>
2023-09-10 21:14:16 +0530

    Add button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+import "./Button.css"
+function Button(){
+    return (
+        <div className="buttons">
+            <button className="button buttonMain">Register</button>
+        </div>
+    )
+}
+
+export default Button;
