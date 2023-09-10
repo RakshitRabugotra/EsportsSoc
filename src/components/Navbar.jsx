52c5b6b12438ed6a73fb86c0b08ecb677b028067
Rakshit Rabugotra <rakshit.rabugotra000@gmail.com>
2023-09-10 12:27:09 +0530

    Change 'class' to 'className' in .jsx file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,41 +7,41 @@ function Navbar() {
 
     return (
       <div>
-      <header class="header">
+      <header className="header">
 
-      <div class="header-top">
-        <div class="container">
+      <div className="header-top">
+        <div className="container">
   
-          <div class="countdown-text">
-            Exclusive Black Friday ! Offer <span class="span skewBg">10</span> Days
+          <div className="countdown-text">
+            Exclusive Black Friday ! Offer <span className="span skewBg">10</span> Days
           </div>
   
-          <div class="social-wrapper">
+          <div className="social-wrapper">
   
-            <p class="social-title">Follow us on :</p>
+            <p className="social-title">Follow us on :</p>
   
-            <ul class="social-list">
+            <ul className="social-list">
   
               <li>
-                <a href="#" class="social-link">
+                <a href="#" className="social-link">
                   <ion-icon name="logo-facebook"></ion-icon>
                 </a>
               </li>
   
               <li>
-                <a href="#" class="social-link">
+                <a href="#" className="social-link">
                   <ion-icon name="logo-twitter"></ion-icon>
                 </a>
               </li>
   
               <li>
-                <a href="#" class="social-link">
+                <a href="#" className="social-link">
                   <ion-icon name="logo-pinterest"></ion-icon>
                 </a>
               </li>
   
               <li>
-                <a href="#" class="social-link">
+                <a href="#" className="social-link">
                   <ion-icon name="logo-linkedin"></ion-icon>
                 </a>
               </li>
@@ -53,56 +53,56 @@ function Navbar() {
         </div>
       </div>
   
-      <div class="header-bottom skewBg" data-header>
-        <div class="container">
+      <div className="header-bottom skewBg" data-header>
+        <div className="container">
   
-          <a href="#" class="logo">EsportsSoc</a>
+          <a href="#" className="logo">EsportsSoc</a>
   
-          <nav class="navbar" data-navbar>
-            <ul class="navbar-list">
+          <nav className="navbar" data-navbar>
+            <ul className="navbar-list">
   
-              <li class="navbar-item">
-                <a href="#home" class="navbar-link skewBg" data-nav-link>Home</a>
+              <li className="navbar-item">
+                <a href="#home" className="navbar-link skewBg" data-nav-link>Home</a>
               </li>
   
-              <li class="navbar-item">
-                <a href="#live" class="navbar-link skewBg" data-nav-link>Live</a>
+              <li className="navbar-item">
+                <a href="#live" className="navbar-link skewBg" data-nav-link>Live</a>
               </li>
   
-              <li class="navbar-item">
-                <a href="#features" class="navbar-link skewBg" data-nav-link>Features</a>
+              <li className="navbar-item">
+                <a href="#features" className="navbar-link skewBg" data-nav-link>Features</a>
               </li>
   
-              <li class="navbar-item">
-                <a href="#shop" class="navbar-link skewBg" data-nav-link>Shop</a>
+              <li className="navbar-item">
+                <a href="#shop" className="navbar-link skewBg" data-nav-link>Shop</a>
               </li>
   
-              <li class="navbar-item">
-                <a href="#blog" class="navbar-link skewBg" data-nav-link>Blog</a>
+              <li className="navbar-item">
+                <a href="#blog" className="navbar-link skewBg" data-nav-link>Blog</a>
               </li>
   
-              <li class="navbar-item">
-                <a href="#" class="navbar-link skewBg" data-nav-link>Contact</a>
+              <li className="navbar-item">
+                <a href="#" className="navbar-link skewBg" data-nav-link>Contact</a>
               </li>
   
             </ul>
           </nav>
   
-          <div class="header-actions">
+          <div className="header-actions">
   
-            <button class="cart-btn" aria-label="cart">
+            <button className="cart-btn" aria-label="cart">
               <ion-icon name="cart"></ion-icon>
   
-              <span class="cart-badge">0</span>
+              <span className="cart-badge">0</span>
             </button>
   
-            <button class="search-btn" aria-label="open search" data-search-toggler>
+            <button className="search-btn" aria-label="open search" data-search-toggler>
               <ion-icon name="search-outline"></ion-icon>
             </button>
   
-            <button class="nav-toggle-btn" aria-label="toggle menu" data-nav-toggler>
-              <ion-icon name="menu-outline" class="menu"></ion-icon>
-              <ion-icon name="close-outline" class="close"></ion-icon>
+            <button className="nav-toggle-btn" aria-label="toggle menu" data-nav-toggler>
+              <ion-icon name="menu-outline" className="menu"></ion-icon>
+              <ion-icon name="close-outline" className="close"></ion-icon>
             </button>
   
           </div>
@@ -116,16 +116,16 @@ function Navbar() {
   
   
   
-    <div class="search-container" data-search-box>
+    <div className="search-container" data-search-box>
   
-      <div class="input-wrapper">
-        <input type="search" name="search" aria-label="search" placeholder="Search here..." class="search-field"/>
+      <div className="input-wrapper">
+        <input type="search" name="search" aria-label="search" placeholder="Search here..." className="search-field"/>
   
-        <button class="search-submit" aria-label="submit search" data-search-toggler>
+        <button className="search-submit" aria-label="submit search" data-search-toggler>
           <ion-icon name="search-outline"></ion-icon>
         </button>
   
-        <button class="search-close" aria-label="close search" data-search-toggler></button>
+        <button className="search-close" aria-label="close search" data-search-toggler></button>
       </div>
   
     </div>
