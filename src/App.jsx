9f7b71efebc4cbd6dd91a0bc5dcf694f7759a397
Rakshit Rabugotra <rakshit.rabugotra000@gmail.com>
2023-09-13 12:23:25 +0530

    Refactor styles and add images to image-slider

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,24 @@ import Navbar from "./components/Navbar/Navbar";
 import Gallery from "./components/Gallery/Gallery";
 import ImageSlider from "./components/ImageSlider/ImageSlider";
 
+import codmImage from "./assets/games/codm.jpg";
 import csgoImage from "./assets/games/csgo.jpg";
 import fallguysImage from "./assets/games/fallguys.jpg";
 import fifa22Image from "./assets/games/fifa22.jpg";
 import forzaImage from "./assets/games/forza.jpg";
-import valorantImage from "./assets/games/valorant.jpg";
+import rocketLeagueImage from "./assets/games/rocket-league.jpg"
 import tekkenImage from "./assets/games/tekken7.jpg";
+import valorantImage from "./assets/games/valorant.jpg";
 
 const slides = [
+  {url: codmImage, title: 'CODM'},
   {url: csgoImage, title: 'csgo'},
   {url: fallguysImage, title: 'fallguys'},
   {url: fifa22Image, title: 'fifa22'},
   {url: forzaImage, title: 'forza'},
+  {url: rocketLeagueImage, title: 'Rocket League'},
+  {url: tekkenImage, title: 'tekken'},
   {url: valorantImage, title: 'valorant'},
-  {url: tekkenImage, title: 'tekken'}
 ];
 
 function App() {
