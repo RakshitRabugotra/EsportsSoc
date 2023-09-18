673cbf3c62164e9943f7f34fcab55cc971f591c2
Rakshit Rabugotra <rakshit.rabugotra000@gmail.com>
2023-09-19 02:57:22 +0530

    Add blog section placeholder

---
+++ src/components/Blog/Blog.jsx
@@ -0,0 +1,20 @@
+import Heading from "../Heading/Heading";
+import "./Blog.css";
+
+const Blog = () => {
+  return (
+    <div>
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}>
+        <Heading>Blog</Heading>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
