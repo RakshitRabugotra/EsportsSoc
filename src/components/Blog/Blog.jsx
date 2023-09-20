a23a36d8a0d311252b163125a9c0fd1b4e578893
Rakshit Rabugotra <rakshit.rabugotra000@gmail.com>
2023-09-20 14:25:34 +0530

    Refurnish the blog section

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,20 +1,45 @@
-import Heading from "../Heading/Heading";
 import "./Blog.css";
+import ContactCard from "../ContactCard/ContactCard";
+
+import contactImage1 from "../../assets/persons/rakshit.jpg";
+import contactImage2 from "../../assets/persons/deepanshu.jpg";
+import contactImage3 from "../../assets/persons/yashmit.jpg";
+import contactImage4 from "../../assets/persons/virendra.jpg";
+import contactImage5 from "../../assets/persons/varun.jpg";
+
+
+const contacts = [
+  {imageURL: contactImage1, personName: "Rakshit Rabugotra"},
+  {imageURL: contactImage2, personName: "Deepanshu Saini"},
+  {imageURL: contactImage3, personName: "Yashmit Sharma"},
+  {imageURL: contactImage4, personName: "Virendra Lal"},
+  {imageURL: contactImage5, personName: "Varun Gaur"},
+]
+
 
 const Blog = () => {
   return (
-    <div>
-      <div
-        style={{
-          width: "100%",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}>
-        <Heading>Blog</Heading>
+    <div className="blog-container">
+      <div className="blog-container__left-segment">
+        <div className="content">
+          <h1 style={{textAlign: "center"}}>About Us</h1>
+        </div>
+      </div>
+  
+      <div className="blog-container__right-segment">
+        <div className="content">
+          <h1>Our Team</h1>
+          <div className="card-holder">
+            {
+              contacts.map((contact, index) => {
+                return <ContactCard imageURL={contact.imageURL} personName={contact.personName} key={index}/>
+              })
+            }
+          </div>
+        </div>
       </div>
     </div>
   );
-};
+}
 
 export default Blog;
